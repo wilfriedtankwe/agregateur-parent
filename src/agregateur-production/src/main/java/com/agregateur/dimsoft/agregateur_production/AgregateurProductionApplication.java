72d6e3a32909d1819f7ba5808c8414a01af67c8e
wilfriedjordan <tankwewilfried5@gmail.com>
2025-10-17 00:14:26 +0100

    Initialisation

---
+++ src/agregateur-production/src/main/java/com/agregateur/dimsoft/agregateur_production/AgregateurProductionApplication.java
@@ -0,0 +1,13 @@
+package com.agregateur.dimsoft.agregateur_production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregateurProductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregateurProductionApplication.class, args);
+	}
+
+}

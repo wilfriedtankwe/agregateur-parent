64d2fa5548687f3243b5ce071f19d3de3295aae0
LincolBantio <rolinlincoltiotsop@gmail.com>
2025-10-19 13:21:04 +0200

    Recuperation du numero de compte et de la bank_id

--- src/agregateur-production/src/main/java/com/agregateur/dimsoft/agregateur_production/controller/ImportFileController.java
+++ src/agregateur-production/src/main/java/com/agregateur/dimsoft/agregateur_production/controller/ImportFileController.java
@@ -1,8 +1,28 @@
 package com.agregateur.dimsoft.agregateur_production.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import com.agregateur.dimsoft.agregateur_production.services.impl.ImportFileServiceImpl;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
 
 @RestController
+@RequestMapping("/api/files")
 public class ImportFileController {
+
+    private final ImportFileServiceImpl importFileService;
+
+    public ImportFileController(ImportFileServiceImpl importFileService) {
+        this.importFileService = importFileService;
+    }
+
+
+
+    @GetMapping("/extract-account-number")
+    public Map<String, ImportFileServiceImpl.AccountResult> extractNumbers() {
+        return importFileService.extractNumbersFromFiles();
+    }
+
+
 
 }

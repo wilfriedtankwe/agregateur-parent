5b1672a1e11e6e615d6b7f17cde8d97aaf7505ca
LincolBantio <rolinlincoltiotsop@gmail.com>
2025-10-19 13:21:04 +0200

    Recuperation du numero de compte et de la bank_id

--- src/agregateur-production/src/main/java/com/agregateur/dimsoft/agregateur_production/services/ImportFileService.java
+++ src/agregateur-production/src/main/java/com/agregateur/dimsoft/agregateur_production/services/ImportFileService.java
@@ -1,4 +1,14 @@
 package com.agregateur.dimsoft.agregateur_production.services;
 
+import com.agregateur.dimsoft.agregateur_production.services.impl.ImportFileServiceImpl;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public interface ImportFileService {
+    Map<String, ImportFileServiceImpl.AccountResult> extractNumbersFromFiles() ;
+
+
 }
